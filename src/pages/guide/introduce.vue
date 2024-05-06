dcb1c3b0a0799d9aa1b42afecb9713062636f9ad
redgoose <scripter@me.com>
2024-05-06 19:03:38 +0900

    가이드 화면 대략적으로 만듬

--- src/pages/guide/introduce.vue
+++ src/pages/guide/introduce.vue
@@ -1,5 +1,36 @@
 <template>
-<article>
-  <h1>guide / introduce</h1>
+<article class="page">
+  <header class="page-header">
+    <h1>
+      <small>Guide</small>
+      <strong>Development guide</strong>
+    </h1>
+    <p>바구니 앱에서 사용되는 요소들 가이드</p>
+  </header>
+  <div class="page-content">
+    <div class="content-body">
+      <p>이 컨텐츠는 바구니를 만들면서 사용해왔던 공통 컴포넌트들을 편하게 확인하면서 활용할 수 있도록 도움을 주는 영역이라고 할 수 있습이다.</p>
+    </div>
+  </div>
 </article>
 </template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+@use '../../assets/scss/mixins';
+@import './page';
+.content-body {
+  font-size: 14px;
+  line-height: 1.42;
+  > p {
+    &:first-child {
+      margin-top: 0;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
